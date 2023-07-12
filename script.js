82328410c20984bf4b942e70588c334dfef1aae8
Arkana Andoko <arkana.a.andoko@gmail.com>
2023-07-13 02:37:58 +0700

    commit awal

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+    $(".btn").fadeOut();
+    $(".btn").fadeIn(20000);
+    $(".button1").attr("class", "button1 btn");
+    $(".button2").attr("class", "button1 btn");
+    $(".button3").attr("class", "button1 btn");
+    $(".button4").attr("class", "button1 btn");
+    $(".button5").attr("class", "button1 btn");
+    $(".button6").attr("class", "button1 btn");
+});
